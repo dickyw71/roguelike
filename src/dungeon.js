58ecd529e0257e9c5ad2054fe1b2612d6530965d
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-06-14 17:29:41 +0100

    Added DungeonGrid and DungeonCell components.

--- src/dungeon.js
+++ src/dungeon.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 
 class DungeonGrid extends Component {
-
-
+    constructor(props) {
+      super(props);
+    }
     render() {
         // generate board components
         let gridOfCells = this.props.board.map( (rowOfCells) => {
             return rowOfCells.map((cell) => {
-                return <DungeonCell cell={cell} toggleCell={this.props.toggleCell}/>
+                return <DungeonCell cell={cell}  />
             })
         })
 
         return (
-            <div style={this.props.style}>
+            <svg style={this.props.style}>
                 {gridOfCells}   
-            </div>
+            </svg>
         )
     }
 }
 
 class DungeonCell extends Component {
-    
+    constructor(props) {
+        super(props);
+    }    
     
     render() {
-        <svg fill="black"></svg>
+        return (
+            <rect 
+                x={this.props.cell.x*10} 
+                y={this.props.cell.y*10} 
+                width={"10px"} 
+                height={"10px"} 
+                fill="black">
+            </rect>
+        )
     }
 }
+
+export default DungeonGrid;
