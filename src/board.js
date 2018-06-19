19b7219b044e0b3461b086b257dd41f5fd3d2768
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-06-19 08:00:04 +0100

    Removed isAlive value when creating Cell.

--- src/board.js
+++ src/board.js
@@ -6,7 +6,7 @@ export function generateEmpty(height, width) {
     for (let i = 0; i < (height || 5); i++) {
         cellGrid[i] = new Array(width || 5);
         for (let j = 0; j < (width || 5); j++) {
-             cellGrid[i][j] = new Cell(j, i, false);
+             cellGrid[i][j] = new Cell(j, i);
         }
      }
      return cellGrid;
